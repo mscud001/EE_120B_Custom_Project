44d27124177684a3f184130ed98bf4bf28c92bd2
mscud001 <36316697+mscud001@users.noreply.github.com>
2018-02-13 14:38:31 -0800

    Add files via upload

---
+++ mscud001_Space_Invaders.c
@@ -0,0 +1,51 @@
+/*
+ * mscud001_Space_Invaders.c
+ *
+ * Created: 2/13/2018 1:15:06 PM
+ *  Author: Matthew Scudder
+ */ 
+
+#include <avr/io.h>
+#include "io.c"
+
+unsigned long highScore;
+
+void displayHighScoreAndCurrentScore()
+{
+			LCD_Cursor(1);
+			LCD_WriteData('H');
+			LCD_Cursor(2);
+			LCD_WriteData('i');
+			LCD_Cursor(3);
+			LCD_WriteData('g');
+			LCD_Cursor(4);
+			LCD_WriteData('h');
+			LCD_Cursor(5);
+			LCD_WriteData(':');
+			LCD_Cursor(17);
+			LCD_WriteData('S');
+			LCD_Cursor(18);
+			LCD_WriteData('c');
+			LCD_Cursor(19);
+			LCD_WriteData('o');
+			LCD_Cursor(20);
+			LCD_WriteData('r');
+			LCD_Cursor(21);
+			LCD_WriteData('e');
+			LCD_Cursor(22);
+			LCD_WriteData(':');
+}
+
+int main(void)
+{
+	DDRC = 0xFF; PORTC = 0x00; // LCD data lines
+	DDRD = 0xFF; PORTD = 0x00; // LCD control lines
+	
+	LCD_init(); // Initializes the LCD display
+	displayHighScoreAndCurrentScore(); 	// Starting at position 1 and 17 on the LCD screen, writes "High:" and "Score:" to screen.
+	
+	while(1) 
+	{
+		continue;
+	}
+}
